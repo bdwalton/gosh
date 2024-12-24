266d0df80ef1eb52a810660f67ed7005c1b4aedd
Ben Walton <bdwalton@gmail.com>
2024-12-24 15:09:55 +0000

    Add tests for nonce handling

---
+++ network/nonce_test.go
@@ -0,0 +1,153 @@
+package network
+
+import (
+	"math"
+	"slices"
+	"testing"
+)
+
+func TestNextGCMNonceDifferentServerClient(t *testing.T) {
+	cases := []uint64{
+		0,
+		uint64(math.MaxUint32 - 1),
+	}
+
+	for i, c := range cases {
+		n1, n2 := nonce(c), nonce(c)
+		s1, s2 := n1.nextGCMNonce(CLIENT), n2.nextGCMNonce(SERVER)
+		if slices.Equal(s1, s2) {
+			t.Errorf("%d: %v = %v", i, s1, s2)
+		}
+
+		if n1 != n2 {
+			t.Errorf("%d: Underlying nonces not the same: %d and %d", i, n1, n2)
+		}
+	}
+
+}
+
+func TestNextGCMNoncePanicClient(t *testing.T) {
+	// Defer a function to recover from panic
+	defer func() {
+		if r := recover(); r != nil {
+			// We successfully recovered from panic
+			t.Log("Test passed, panic was caught!")
+		}
+	}()
+
+	n := nonce(uint64(math.MaxUint32))
+	// Should panic
+	n.nextGCMNonce(CLIENT)
+
+	t.Errorf("nextGCMNonce() didn't panic when rolling over 32 bits.")
+}
+
+func TestNonceFromBytesPanic32BitsClient(t *testing.T) {
+	// Defer a function to recover from panic
+	defer func() {
+		if r := recover(); r != nil {
+			// We successfully recovered from panic
+			t.Log("Test passed, panic was caught!")
+		}
+	}()
+
+	// Should panic
+	nonceFromBytes([]byte{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0})
+
+	t.Errorf("nonceFromBytes() didn't panic when over 32 bits.")
+}
+
+func TestNextGCMNoncePanicServer(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			// We successfully recovered from panic
+			t.Log("Test passed, panic was caught!")
+		}
+	}()
+
+	n := nonce(uint64(math.MaxUint32))
+	// Should panic
+	n.nextGCMNonce(SERVER)
+
+	t.Errorf("nextGCMNonce didn't panic when rolling over 32 bits.")
+}
+
+func TestNonceFromBytesPanic32BitsServer(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			// We successfully recovered from panic
+			t.Log("Test passed, panic was caught!")
+		}
+	}()
+
+	// Should panic
+	nonceFromBytes([]byte{1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0})
+
+	// If the panic was not caught, the test will fail
+	t.Errorf("nonceFromBytes() didn't panic when over 32 bits.")
+}
+
+func TestNonceFromBytes(t *testing.T) {
+	cases := []struct {
+		bytes []byte
+		want  nonce
+	}{
+		{[]byte{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, nonce(0)},
+		{[]byte{1, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0}, nonce(255)},
+	}
+
+	for i, c := range cases {
+		got := nonceFromBytes(c.bytes)
+		if got != c.want {
+			t.Errorf("%d: Got %d, wanted %d", i, got, c.want)
+		}
+	}
+}
+
+func TestNonceIncrement(t *testing.T) {
+	cases := []struct {
+		n    nonce
+		want []uint64
+	}{
+		{nonce(0), []uint64{1, 2, 3}},
+		{nonce(uint64(math.MaxUint32) - 1), []uint64{uint64(math.MaxUint32)}},
+		{nonce(255), []uint64{256, 257}},
+	}
+
+	for i, c := range cases {
+		for j, n := range c.want {
+			c.n.nextGCMNonce(CLIENT)
+			if c.n != nonce(n) {
+				t.Errorf("%d/%d: Got %d, wanted %d", i, j, c.n, n)
+			}
+		}
+	}
+}
+
+func TestNonceFromBytesWithInvalidInputShort(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			// We successfully recovered from panic
+			t.Log("Test passed, panic was caught!")
+		}
+	}()
+
+	// Should panic
+	nonceFromBytes([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11})
+
+	t.Errorf("nonceFromBytes() didn't panic when given 11 bytes instead of 12.")
+}
+
+func TestNonceFromBytesWithInvalidInputLong(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			// We successfully recovered from panic
+			t.Log("Test passed, panic was caught!")
+		}
+	}()
+
+	// Should panic
+	nonceFromBytes([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13})
+
+	t.Errorf("nonceFromBytes() didn't panic when given 13 bytes instead of 12.")
+}
