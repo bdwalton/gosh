07ce9220a195fe1df84392add2bbde9ce0a02d95
Ben Walton <bdwalton@gmail.com>
2025-01-03 13:47:34 +0000

    Add some basics of the terminal object and initial tests.

---
+++ vt/terminal.go
@@ -0,0 +1,148 @@
+package vt
+
+import (
+	"log/slog"
+	"strings"
+)
+
+type terminal struct {
+	// Functional members
+	p  *parser
+	fb *framebuffer
+
+	// State
+	title, icon string
+	curX, curY  int
+	curF        format
+
+	// Temp
+	oscTemp []rune
+}
+
+func NewTerminal(rows, cols int) *terminal {
+	t := &terminal{
+		fb:      newFramebuffer(rows, cols),
+		oscTemp: make([]rune, 0),
+	}
+	t.p = newParser(t)
+
+	return t
+}
+
+func (t *terminal) handle(action pAction, params []int, data []rune, lastbyte byte) {
+	switch action {
+	case VTPARSE_ACTION_EXECUTE:
+		t.handleExecute(lastbyte)
+	case VTPARSE_ACTION_CSI_DISPATCH:
+		t.handleCSI(params, data, lastbyte)
+	case VTPARSE_ACTION_OSC_PUT, VTPARSE_ACTION_OSC_END:
+		t.handleOSC(action, lastbyte)
+	}
+}
+
+func (t *terminal) handleOSC(act pAction, lastbyte byte) {
+	switch act {
+	case VTPARSE_ACTION_OSC_PUT:
+		t.oscTemp = append(t.oscTemp, rune(lastbyte))
+	case VTPARSE_ACTION_OSC_END:
+		if len(t.oscTemp) > 0 {
+			parts := strings.SplitN(string(t.oscTemp), ";", 2)
+			switch parts[0] {
+			case "0":
+				t.title = parts[1]
+				t.icon = parts[1]
+			case "1":
+				t.icon = parts[1]
+			case "2":
+				t.title = parts[1]
+			}
+			t.oscTemp = t.oscTemp[:0]
+		}
+	}
+}
+
+func (t *terminal) print(r rune) {
+	t.fb.setCell(t.curY, t.curX, t.curF, r)
+}
+
+func (t *terminal) handleExecute(lastbyte byte) {
+	switch lastbyte {
+	case CTRL_BS:
+		t.curX -= 1
+	case CTRL_CR:
+		t.curX = 0
+	case CTRL_HTS:
+
+	}
+}
+
+func (t *terminal) handleCSI(params []int, data []rune, lastbyte byte) {
+	switch lastbyte {
+	case CSI_CUP, CSI_CUD, CSI_CUB, CSI_CUF, CSI_CNL, CSI_CPL, CSI_CHA, CSI_HVP:
+		t.cursorMove(params, lastbyte)
+	case CSI_SGR:
+		t.curF = formatFromParams(t.curF, params)
+	default:
+		slog.Debug("unimplemented CSI code", "lastbyte", lastbyte, "params", params, "data", data)
+	}
+}
+
+func (t *terminal) cursorMove(params []int, moveType byte) {
+	// No paramter indicates a 0 paramter, but for cursor
+	// movement, we always default to 1. That allows more
+	// efficient specification of the common movements.
+	n := 1
+	m := 1
+	if len(params) > 0 {
+		n = params[0]
+	}
+	if len(params) > 1 {
+		m = params[1]
+	}
+
+	row := t.curY
+	col := t.curX
+
+	switch moveType {
+	case CSI_CUU:
+		row -= n
+	case CSI_CUD:
+		row += n
+	case CSI_CUB:
+		col -= n
+	case CSI_CUF:
+		col += n
+	case CSI_CNL:
+		col = 0
+		row += n
+	case CSI_CPL:
+		col = 0
+		row -= n
+	case CSI_CHA:
+		col = n - 1 // our indexing is zero based
+	case CSI_CUP, CSI_HVP: // TODO: What does "format effector" mean for HVP
+		row = n - 1 // out indexing is zero based
+		col = m - 1
+	}
+
+	t.cursorMoveAbs(row, col)
+}
+
+func (t *terminal) cursorMoveAbs(row, col int) {
+	t.curX = col
+	t.curY = row
+
+	switch {
+	case t.curX < 0:
+		t.curX = 0
+	case t.curX >= t.fb.cols:
+		t.curX = t.fb.cols - 1
+	}
+
+	switch {
+	case t.curY < 0:
+		t.curY = 0
+	case t.curY >= t.fb.rows:
+		t.curY = t.fb.rows - 1
+	}
+}
---
+++ vt/terminal_test.go
@@ -0,0 +1,74 @@
+package vt
+
+import (
+	"testing"
+)
+
+func TestCursorMove(t *testing.T) {
+	fb1 := newFramebuffer(24, 80)
+
+	cases := []struct {
+		t            *terminal
+		params       []int
+		mt           byte // move type
+		wantY, wantX int
+	}{
+		// CUU - cursor up
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{}, CSI_CUU, 0, 0},
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{2}, CSI_CUU, 0, 0},
+		{&terminal{fb: fb1, curY: 10, curX: 0}, []int{}, CSI_CUU, 9, 0},
+		{&terminal{fb: fb1, curY: 10, curX: 0}, []int{2}, CSI_CUU, 8, 0},
+		// CUD - cursor down
+		{&terminal{fb: fb1, curY: fb1.rows - 1, curX: 1}, []int{}, CSI_CUD, fb1.rows - 1, 1},
+		{&terminal{fb: fb1, curY: fb1.rows - 1, curX: 1}, []int{3}, CSI_CUD, fb1.rows - 1, 1},
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{}, CSI_CUD, 1, 0},
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{3}, CSI_CUD, 3, 0},
+		// CUB - cursor back
+		{&terminal{fb: fb1, curY: 15, curX: 0}, []int{}, CSI_CUB, 15, 0},
+		{&terminal{fb: fb1, curY: 15, curX: 0}, []int{2}, CSI_CUB, 15, 0},
+		{&terminal{fb: fb1, curY: 15, curX: 3}, []int{2}, CSI_CUB, 15, 1},
+		{&terminal{fb: fb1, curY: 15, curX: 79}, []int{}, CSI_CUB, 15, 78},
+		// CUF - cursor forward
+		{&terminal{fb: fb1, curY: 15, curX: fb1.cols - 1}, []int{}, CSI_CUF, 15, fb1.cols - 1},
+		{&terminal{fb: fb1, curY: 15, curX: fb1.cols - 1}, []int{10}, CSI_CUF, 15, fb1.cols - 1},
+		{&terminal{fb: fb1, curY: 15, curX: 0}, []int{}, CSI_CUF, 15, 1},
+		{&terminal{fb: fb1, curY: 15, curX: 0}, []int{10}, CSI_CUF, 15, 10},
+		// CPL - previous line
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{}, CSI_CPL, 0, 0},
+		{&terminal{fb: fb1, curY: 0, curX: 10}, []int{20}, CSI_CPL, 0, 0},
+		{&terminal{fb: fb1, curY: 15, curX: 20}, []int{}, CSI_CPL, 14, 0},
+		{&terminal{fb: fb1, curY: 21, curX: 10}, []int{20}, CSI_CPL, 1, 0},
+
+		// CNL - next line
+		{&terminal{fb: fb1, curY: fb1.rows - 1, curX: 0}, []int{}, CSI_CNL, fb1.rows - 1, 0},
+		{&terminal{fb: fb1, curY: fb1.rows - 1, curX: 0}, []int{2}, CSI_CNL, fb1.rows - 1, 0},
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{}, CSI_CNL, 1, 0},
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{10}, CSI_CNL, 10, 0},
+		// CHA - cursor horizontal absolute
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{}, CSI_CHA, 0, 0},
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{10}, CSI_CHA, 0, 9},  // 0 vs 1 based
+		{&terminal{fb: fb1, curY: 0, curX: 10}, []int{10}, CSI_CHA, 0, 9}, // 0 vs 1 based
+		{&terminal{fb: fb1, curY: 0, curX: 10}, []int{fb1.cols + 10}, CSI_CHA, 0, fb1.cols - 1},
+		// CUP - cursor position
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{}, CSI_CUP, 0, 0},
+		{&terminal{fb: fb1, curY: 10, curX: 25}, []int{}, CSI_CUP, 0, 0},
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{}, CSI_CUP, 0, 0},
+		{&terminal{fb: fb1, curY: 10, curX: 25}, []int{0, 16}, CSI_CUP, 0, 15},
+		{&terminal{fb: fb1, curY: 10, curX: 25}, []int{16}, CSI_CUP, 15, 0},
+		{&terminal{fb: fb1, curY: 10, curX: 25}, []int{1000, 1000}, CSI_CUP, fb1.rows - 1, fb1.cols - 1},
+		// HVP - horizontal vertical position
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{}, CSI_HVP, 0, 0},
+		{&terminal{fb: fb1, curY: 10, curX: 25}, []int{}, CSI_HVP, 0, 0},
+		{&terminal{fb: fb1, curY: 0, curX: 0}, []int{}, CSI_HVP, 0, 0},
+		{&terminal{fb: fb1, curY: 10, curX: 25}, []int{0, 16}, CSI_HVP, 0, 15},
+		{&terminal{fb: fb1, curY: 10, curX: 25}, []int{16}, CSI_HVP, 15, 0},
+		{&terminal{fb: fb1, curY: 10, curX: 25}, []int{1000, 1000}, CSI_HVP, fb1.rows - 1, fb1.cols - 1},
+	}
+
+	for i, c := range cases {
+		c.t.cursorMove(c.params, c.mt)
+		if c.t.curX != c.wantX || c.t.curY != c.wantY {
+			t.Errorf("%d: Got (r: %d, c: %d), wanted (r: %d, c: %d)", i, c.t.curY, c.t.curX, c.wantY, c.wantX)
+		}
+	}
+}
