2ff9490e10d61bd8a3467b7b52b596ec80d70b5c
Ben Walton <bdwalton@gmail.com>
2025-04-17 17:04:43 +0100

    Move cursor tests to cursor_test file
    Purely for consistency with where the main code logic is.

--- vt/terminal_test.go
+++ vt/terminal_test.go
@@ -218,24 +218,6 @@ func TestCarriageReturn(t *testing.T) {
 	}
 }
 
-func TestCursorMoveToAnsi(t *testing.T) {
-	cases := []struct {
-		cur  cursor
-		want string
-	}{
-		{cursor{0, 0}, fmt.Sprintf("%c%c%c", ESC, ESC_CSI, CSI_CUP)},
-		{cursor{1, 1}, fmt.Sprintf("%c%c2;2%c", ESC, ESC_CSI, CSI_CUP)},
-		{cursor{30, 0}, fmt.Sprintf("%c%c31%c", ESC, ESC_CSI, CSI_CUP)},
-		{cursor{0, 15}, fmt.Sprintf("%c%c;16%c", ESC, ESC_CSI, CSI_CUP)},
-	}
-
-	for i, c := range cases {
-		if got := c.cur.getMoveToAnsi(); got != c.want {
-			t.Errorf("%d: Got %q, wanted %q", i, got, c.want)
-		}
-	}
-}
-
 func testTerminalCopy(term *Terminal) *Terminal {
 	c := term.Copy()
 	c.lastChg = time.Now()
